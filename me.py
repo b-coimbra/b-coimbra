878225896544023a6f27fa4aab6740e2949c6e97
Bruno Coimbra <b-coimbra@hotmail.com>
2021-07-17 00:15:59 -0300

    Update me.py

--- me.py
+++ me.py
@@ -1,9 +1,7 @@
-print("hello, nice to meet you!")
-
 name = {'Bruno Coimbra', 'b-coimbra'}
 bio = 'jr. software dev'
 website = 'metaphoric.dev'
 email = 'me@metaphoric.dev'
-fingerprint = {'pgp': '0xE1082693'}
+fingerprint = {'pgp': '0xE1082693', 'eth': '0xFC55C2FF94f990d702e535B59E4284417257C9aB'}
 interests = ['literature', 'post punk', 'rhythm games']
 learning = ['design patterns', 'systems architecture', 'cloud computing', 'docker']
